9e332c3f8e533451106e47bf2cc3ef779a351214
Emanuel Albuqueruque <emanuel.albuquerque.1995@gmail.com>
2025-07-03 07:02:34 -0300

    Novas imagens para uma nova ideia

--- run_multiple.py
+++ run_multiple.py
@@ -8,10 +8,20 @@ if __name__ == "__main__":
     # Processo 2: Teclas W (pular) e S (abaixar)
     p2 = multiprocessing.Process(target=dino_game.menu, args=(0, dino_game.pygame.K_w, dino_game.pygame.K_s))
 
+    # Processo 3: Teclas E (pular) e D (abaixar)
+    p3 = multiprocessing.Process(target=dino_game.menu, args=(0, dino_game.pygame.K_e, dino_game.pygame.K_d))
+
+    # Processo 4: Teclas R (pular) e F (abaixar)
+    p4 = multiprocessing.Process(target=dino_game.menu, args=(0, dino_game.pygame.K_r, dino_game.pygame.K_f))
+
     # Iniciar os dois processos
     p1.start()
     p2.start()
+    p3.start()
+    p4.start()
 
     # Esperar os dois terminarem
     p1.join()
     p2.join()
+    p3.join()
+    p4.join()
